7621d68c3691f32ad6c931db267a78597c425897
Mustafa Ikbal Mehmedoglu <mikbalm@gmail.com>
2025-05-10 01:19:48 -0400

    code-along!

--- lib/testing/course_test.py
+++ lib/testing/course_test.py
@@ -3,19 +3,18 @@ from Course import Course
 class Test_Course:
     '''Course in Course.py'''
 
-
-
+    # fixed test methods.
     def test_title_instance_variable(self):
         "Test setting and getting for title"
-        course = Course(title="Programming Robots for Outer Space")
-        assert(course.title == "Programming Robots for Outer Space")
+        course = Course(title="Programming Robots for Outer Space", schedule="Full-Time", description="Learn robotics")
+        assert course.title == "Programming Robots for Outer Space"
 
     def test_schedule_instance_variable(self):
         "Test setting and getting for schedule"
-        course = Course(schedule="Full-Time")
-        assert(course.schedule == "Full-Time")
+        course = Course(title="Intro to AI", schedule="Full-Time", description="Learn about artificial intelligence")
+        assert course.schedule == "Full-Time"
 
     def test_description_instance_variable(self):
         "Test setting and getting for description"
-        course = Course(description="Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
-        assert(course.description == "Learn how to program robots to explore the depths of space. Guest lecturer: The Mars Rover")
+        course = Course(title="Space Navigation", schedule="Part-Time", description="Learn how to navigate in space")
+        assert course.description == "Learn how to navigate in space"
